b4ce2ebae7a0c14d05cca642a8991a0e40b877f8
Max Vecherinsky <47793074+BittahLover@users.noreply.github.com>
2020-04-30 14:37:01 +0300

    добавлен class

--- lab2/Task3/task3.py
+++ lab2/Task3/task3.py
@@ -1,47 +1,46 @@
-def add_vectors(v, w):
-    return [vi + wi for vi, wi in zip(v, w)]
+class Vector:
+    def __init__(self, v, w, c):
+        self.w = w
+        self.v = v
+        self.c = c
+
+    def add_vectors(self, v, w):
+        return [vi + wi for vi, wi in zip(v, w)]
+
+    def sub_vectors(self, v, w):
+        return [vi - wi for vi, wi in zip(v, w)]
+
+    def constant_multiply(self, c, v):
+        return [c * vi for vi in v]
+
+    def scalar_multiply(self, v, w):
+        return sum(vi * wi for vi, wi in zip(v, w))
+
+    def len_vector(self, v):
+        return len(v)
+
+    def index_elem(self, c, v):
+        return v[c]
+
+    def stroka_vector(self, v):
+        return ' '.join(str(e) for e in v)
+
+    def compare(self, v, w, true='equal', false='non_equal', ):
+        for i in range(self.len_vector(v)):
+            if self.index_elem(i, v) == self.index_elem(i, w):
+                continue
+            elif self.index_elem(i, v) != self.index_elem(i, w):
+                return false
+        return true
 
 
-def sub_vectors(v, w):
-    return [vi - wi for vi, wi in zip(v, w)]
+vector1 = Vector([3, 5, 6], [3, 5, 10], 20)
+print(vector1.sub_vectors([3, 5, 6], [3, 5, 10]))
+print(vector1.add_vectors([3, 5, 6], [3, 5, 10]))
+print(vector1.constant_multiply(20, [3, 5, 6]))
+print(vector1.scalar_multiply([3, 5, 6], [3, 5, 10]))
+print(vector1.len_vector([3, 5, 6]))
+print(vector1.index_elem(2, [3, 5, 6]))
+print(vector1.stroka_vector([3, 5, 6]))
+print(vector1.compare([3, 5, 6], [3, 5, 10], true='equal', false="non_equal"))
 
-
-def constant_multiply(c, v):
-    return [c * vi for vi in v]
-
-
-def scalar_multiply(v, w):
-    return sum(vi * wi for vi, wi in zip(v, w))
-
-
-def len_vector(v):
-    return len(v)
-
-
-def index_elem(c, v):
-    return v[c]
-
-
-def stroka_vector(v):
-    return ' '.join(str(e) for e in v)
-
-
-def compare(v, w, true='equal', false='non_equal', ):
-    for i in range(len_vector(v)):
-        if index_elem(i, v) == index_elem(i, w):
-            continue
-        elif index_elem(i, v) != index_elem(i, w):
-            return false
-    return true
-
-
-python_list_1 = [3, 5, 6]
-python_list_2 = [3, 5, 10]
-print(constant_multiply(20, python_list_2))
-print(add_vectors(python_list_1, python_list_2))
-print(sub_vectors(python_list_1, python_list_2))
-print(scalar_multiply(python_list_1, python_list_2))
-print(len_vector(python_list_2))
-print(index_elem(2, python_list_1))
-print(stroka_vector(python_list_1))
-print(compare(python_list_2, python_list_1))
